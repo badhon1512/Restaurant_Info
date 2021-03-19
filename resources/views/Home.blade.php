642a977c0c6c8623a283bbaeae4762264aaf2869
badhon1512 <badhon1512@gmail.com>
2021-03-19 11:15:36 +0600

    Basic project setup adn layout have been added

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,7 @@
+@extends('Layout')
+
+@section('content')
+
+<h1>home page</h1>
+    
+@endsection
---
+++ resources/views/Layout.blade.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Restaurant App</title>
+  </head>
+
+  <body>
+    <header>
+     This is header
+    </header>
+
+
+    <div>
+      @yield('content')
+    </div>
+
+
+    <footer>
+
+      Footer will be here
+
+    </footer>
+  </body>
+</html>
